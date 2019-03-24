83f956099ffe60f26b586612a73d870fd3465025
Awais512 <raza.awais25@gmail.com>
2019-03-24 09:57:07 +0500

    First Commit

---
+++ accounts/views.py
@@ -0,0 +1,64 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages, auth
+from django.contrib.auth.models import User
+
+def register(request):
+    if request.method == 'POST':
+        # get form values
+        first_name = request.POST['firstName']
+        last_name = request.POST['lastName']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                messages.error(request, 'Usernme already taken')
+                return redirect('register')
+            else:
+                if User.objects.filter(email=email).exists():
+                    messages.error(request, 'Email already exists')
+                    return redirect('register')
+                else:
+                    user = User.objects.create_user(username=username, password=password, email=email, first_name=first_name, last_name= last_name)
+                    user.save()
+                    messages.success(request, 'You are register now and you can login')
+                    return redirect('login')
+        else:
+            messages.error(request, 'Password Do not Match')
+            return redirect('register')
+    else:
+        return render(request, 'accounts/register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password= password)
+
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are successfully logged in ')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Invalid Username or Password')
+            return redirect('login')
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        messages.success(request, 'You are successfully Logout')
+        return redirect('index')
+
+
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+
